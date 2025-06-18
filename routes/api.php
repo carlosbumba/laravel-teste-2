21069b45baa3048ff5ecb32d231058ab62ae0f62
Carlos Bumba <carlosbumbadev16@gmail.com>
2025-06-18 16:31:30 +0100

    feat: cria rota para validação de número

---
+++ routes/api.php
@@ -0,0 +1,6 @@
+<?php
+
+use App\Http\Controllers\PhoneValidationController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/v1/validate-number', [PhoneValidationController::class, 'validate']);

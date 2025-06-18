1872228d905ab3fa7905c1228b0c23ea3d9ed5e0
Carlos Bumba <carlosbumbadev16@gmail.com>
2025-06-18 17:50:09 +0100

    refactor: centralizar validação da API de número em PhoneValidationRequest

--- app/Http/Controllers/PhoneValidationController.php
+++ app/Http/Controllers/PhoneValidationController.php
@@ -2,20 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PhoneValidationRequest;
 use App\Http\Resources\NumVerifyResource;
 use App\Services\NumVerifyService;
-use Illuminate\Http\Request;
 
 class PhoneValidationController extends Controller
 {
     public function __construct(protected NumVerifyService $service) {}
 
-    public function __invoke(Request $request)
+    public function __invoke(PhoneValidationRequest $request)
     {
-        $request->validate([
-            'number' => 'required|string'
-        ]);
-
         $data = $this->service->validate($request->number, $request->country_code);
 
         if (isset($data['error'])) {

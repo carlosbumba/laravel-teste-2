2a80f989e92f8862064764d5baf9dae84242124d
Carlos Bumba <carlosbumbadev16@gmail.com>
2025-06-18 18:42:33 +0100

    chore(routes): nomeia rota de validação de número como 'api.validate-number'

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\PhoneValidationController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('throttle:validate-number')->get('/v1/validate-number', PhoneValidationController::class);
+Route::middleware('throttle:validate-number')->get('/v1/validate-number', PhoneValidationController::class)->name('api.validate-number');

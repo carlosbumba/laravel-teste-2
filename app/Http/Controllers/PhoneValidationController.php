d24b52edc3b0059a8c3fc9c8317c866ace34b540
Carlos Bumba <carlosbumbadev16@gmail.com>
2025-06-18 18:54:21 +0100

    refactor(controlador): adicionar tipos de retorno na action __invoke do validador de telefone

--- app/Http/Controllers/PhoneValidationController.php
+++ app/Http/Controllers/PhoneValidationController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use App\Http\Requests\PhoneValidationRequest;
 use App\Http\Resources\NumVerifyResource;
 use App\Services\NumVerifyService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class PhoneValidationController extends Controller
 {
     public function __construct(protected NumVerifyService $service) {}
 
-    public function __invoke(PhoneValidationRequest $request)
+    public function __invoke(PhoneValidationRequest $request): JsonResource|JsonResponse
     {
         $data = $this->service->validate($request->number, $request->country_code);
 
